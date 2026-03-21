47a394fd5446a1f153e98093c54accb10df357e7
scryv <scryvcor@gmail.com>
2026-03-21 23:27:18 +0100

    Added description for govault command Cobra

--- cmd/govault.go
+++ cmd/govault.go
@@ -15,13 +15,8 @@ import (
 
 var govaultCmd = &cobra.Command{
 	Use:   "govault",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Command that runs govault",
+	Long: `This Command runs govault it will ask for login and password and when logged in will display the stored passwords with email or username`,
 	Run: func(cmd *cobra.Command, args []string) {
 		ruun()
 	},
