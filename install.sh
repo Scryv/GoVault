368b1e4ed1e92a32f55fb3267a6cd3cda12ecff5
scryv <scryvcor@gmail.com>
2026-04-04 23:32:20 +0200

    Added Linux install script

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -euo pipefail
+require_root() {
+  if [[ $EUID -ne 0 ]]; then
+    echo "Error: must run as root"
+    exit 1
+  fi
+}
+
+require_root
+echo "Welcome to GoVault Installer"
+go build
+chmod +x GoVault
+echo "made GoVault Executable"
+sudo mv GoVault /usr/local/bin/
+echo "Copied GoVault to /user/local/bin"
+echo "Install is finished try it out by typing the command gv!"
+
+
